ca6f40cea499d0b8296c52b00834ac71507a57dd
Qiaowei Tang <qiaowei.tang@ad.infosys.com>
2019-06-17 13:46:01 -0500

    set up auth service, interceptor, etc

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	FormBuilder,
+	FormGroup,
+	FormControl,
+	Validators,
+} from '@angular/forms';
+
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserAuthService } from 'src/app/core/services/user-auth.service';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private userService: UserAuthService,
+		private fb: FormBuilder
+	) {}
 
 	ngOnInit() {}
 }
