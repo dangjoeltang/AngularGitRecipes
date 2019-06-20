8e03b20a07b8f0fe518ac3b68de650f49e114ee8
Qiaowei Tang <qiaowei.tang@ad.infosys.com>
2019-06-20 11:50:13 -0500

    greeting page after login

--- src/app/shared/components/landing-page/landing-page.component.ts
+++ src/app/shared/components/landing-page/landing-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserAuthService } from 'src/app/core/services/user-auth.service';
+import { User } from 'src/app/core/models';
+
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+	selector: 'app-landing-page',
+	templateUrl: './landing-page.component.html',
+	styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+	private currentUser: User;
 
-  constructor() { }
+	constructor(private userService: UserAuthService) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.userService.currentUser.subscribe(userData => {
+			this.currentUser = userData;
+		});
+	}
 }
