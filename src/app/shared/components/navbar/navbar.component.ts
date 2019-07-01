a680fa81c37fa00e95b776bbfe5b02a12c19f59e
Qiaowei Tang <qiaowei.tang@ad.infosys.com>
2019-07-01 14:42:04 -0500

    added new recipe button to navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -23,7 +23,6 @@ export class NavbarComponent implements OnInit {
 	ngOnInit() {
 		this.userService.currentUser.subscribe(userData => {
 			this.currentUser = userData;
-			// console.log(this.currentUser);
 		});
 	}
 }
